01eba1d25d6d8238a4de3e57acf6d75d15136e37
Sindre Sorhus <sindresorhus@gmail.com>
2024-12-18 17:26:13 +0100

    Meta tweaks

--- index.test-d.ts
+++ index.test-d.ts
@@ -19,3 +19,44 @@ expectType<string>(result[0]);
 expectType<string>(result[1]);
 expectType<void>(result[2]);
 expectType<number>(result[3]);
+
+{
+	const numbers1 = [1, 2];
+
+	const result1 = await pAll(numbers1.map(
+		n => async () =>
+			Promise.resolve().then(() => ({
+				n,
+			}))), {concurrency: 2});
+
+	expectType<{n: number}>(result1[0]);
+	expectType<{n: number}>(result1[1]);
+}
+
+{
+	const numbers2 = [1, 2];
+
+	const actions2 = numbers2.map(
+		n => async () =>
+			Promise.resolve().then(() => ({
+				n,
+			})));
+
+	const result2 = await pAll(actions2, {concurrency: 2});
+
+	expectType<{n: number}>(result2[0]);
+	expectType<{n: number}>(result2[1]);
+}
+
+// Doesn't work: https://github.com/sindresorhus/p-all/issues/15
+// {
+// 	const numbers3 = [1, 2];
+// 	const result3 = await pAll(numbers3.map(
+// 		n => () =>
+// 			Promise.resolve().then(() => ({
+// 				n,
+// 			}))), {concurrency: 2});
+
+// 	expectType<{n: number}>(result3[0]);
+// 	expectType<{n: number}>(result3[1]);
+// }
